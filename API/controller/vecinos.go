99fca289c38da06f65c9ad832f6f2f446f7444a1
Mariiacristina <mariacristina.binfa@mail.udp.cl>
2018-04-05 12:00:22 -0400

    compila

--- API/controller/vecinos.go
+++ API/controller/vecinos.go
@@ -1,16 +1,18 @@
 package controller
 
 import(
-//  "SIGCO/API/model"
+  "SIGCO/API/model"
   "net/http"
   "log"
-//  "SIGCO/API/schema"
-//  "encoding/json"
+  "SIGCO/API/schema"
+  "encoding/json"
 )
 
 func GetAllVecinos(w http.ResponseWriter, r *http.Request){
   log.Println("get VECINOS")
 }
+
+
 //insertar marciano
 func PostVecino(w http.ResponseWriter, r *http.Request){
   var vecino schema.Vecino
